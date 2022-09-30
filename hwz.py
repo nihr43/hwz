257b384b55a3239997c58be83365ce3609171034
nathan hensel <nathanihensel@gmail.com>
2022-09-29 22:04:42 -0500

    consistency in data types

--- hwz.py
+++ hwz.py
@@ -6,8 +6,10 @@ from flask import jsonify
 from flask_restful import Resource, Api
 
 
-# returns list of dicts parsable by jsonify
 def check_disks():
+    '''
+    returns list containing dicts representing each disk
+    '''
     disks = pySMART.DeviceList()
     json = []
 
@@ -22,14 +24,17 @@ def check_disks():
 
 
 def check_ecc_events():
-    ecc_count = 0
+    '''
+    returns dict containing ecc error count
+    '''
+    ecc_events = {"ecc_error_count": 0}
     with open(r'/var/log/kern.log', 'r') as fp:
         lines = fp.readlines()
         for row in lines:
             word = 'mce: [Hardware Error]: Machine check events logged'
             if row.find(word) == 0:
-                ecc_count += 1
-    return ecc_count
+                ecc_events['ecc_error_count'] += 1
+    return ecc_events
 
 
 if __name__ == '__main__':
@@ -38,9 +43,8 @@ if __name__ == '__main__':
 
     class all_stats(Resource):
         def get(self):
-            disks_output = check_disks()
-            ecc_events = {"ecc_error_count": check_ecc_events()}
-            return(jsonify(disks=disks_output, ecc=ecc_events))
+            return(jsonify(disks=check_disks(),
+                           ecc=check_ecc_events()))
 
     api.add_resource(all_stats, '/healthz/')
     app.run(host='0.0.0.0', port='5000')
